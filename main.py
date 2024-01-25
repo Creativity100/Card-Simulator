0719c14c9355b8562be726932b446fd32f245ddd
Jimmy Liu <jimmyliu20040@gmail.com>
2024-01-25 11:53:31 -0800

    Testing main python

---
+++ main.py
@@ -0,0 +1,8 @@
+def card_simulator(num_of_trials):
+    print(f'the number of trials to run is {num_of_trials}')
+
+
+if __name__ == "__main__":
+    num_of_trials = input("How many trials do you want to run?\n")
+
+    card_simulator(num_of_trials)
