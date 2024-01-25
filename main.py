d2daac38eb4222dd121c7dc812cc7efea0a951b2
Jimmy Liu <jimmyliu20040@gmail.com>
2024-01-25 12:26:49 -0800

    implemented card_simulator

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import copy
+import random
+
 '''
 1 - 13   spades
 14 - 26  hearts
@@ -10,30 +13,47 @@ total_trials = 0
 
 
 def card_simulator(num_of_trials):
+    global count, total_trials
+
     # validate param
     if num_of_trials <= 0:
         print(f'invalid number of trials {num_of_trials}')
         return
 
-    # run n trials
-    count = 0
-    total = 0
+    # run trials
     for i in range(num_of_trials):
-        pass
+        deck = copy.deepcopy(DECK)
+
+        # draw 13 cards without replacement
+        ace_count = 0
+        for j in range(13):
+            random_index = random.randint(1, len(deck))
+            draw = deck.pop(random_index - 1)
+
+            if draw % 13 == 1:
+                ace_count += 1
+
+            if ace_count > 1:
+                break
+
+        # increment
+        count = count + 1 if ace_count == 1 else count
+        total_trials += 1
 
 
 def print_results():
     print("RESULTS:")
     print(f'Total trials run:         {total_trials}')
     print(f'Number of counts:         {count}')
-    print(f'Theoretical probabilitY:  {count / total_trials}')
+    print(f'Theoretical probability:  {count / total_trials}')
 
 
 if __name__ == "__main__":
-    num_of_trials = int(input("How many trials do you want to run?\n"))
+    n = int(input("How many trials do you want to run?\n"))
 
     # fill up deck
     for i in range(52):
         DECK.append(i + 1)
 
-    card_simulator(num_of_trials)
+    card_simulator(n)
+    print_results()
