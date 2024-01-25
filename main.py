41d8afe5b44d3fba829ea11fbcdfbb6ed0b5336e
Jimmy Liu <jimmyliu20040@gmail.com>
2024-01-25 11:56:02 -0800

    Validating params

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def card_simulator(num_of_trials):
-    print(f'the number of trials to run is {num_of_trials}')
+    # validate param
+    if num_of_trials <= 0:
+        print(f'invalid number of trials {num_of_trials}')
+        return
+
+
 
 
 if __name__ == "__main__":
-    num_of_trials = input("How many trials do you want to run?\n")
+    num_of_trials = int(input("How many trials do you want to run?\n"))
 
     card_simulator(num_of_trials)
