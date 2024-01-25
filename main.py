8154f5b2fb204306be32faf293c3a94428264772
Jimmy Liu <jimmyliu20040@gmail.com>
2024-01-25 11:58:24 -0800

    adding the deck of cards

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+'''
+1 - 13   spades
+14 - 26  hearts
+27 - 39  clovers
+40 - 52  diamonds
+'''
+DECK = []
+
 def card_simulator(num_of_trials):
     # validate param
     if num_of_trials <= 0:
@@ -9,5 +17,9 @@ def card_simulator(num_of_trials):
 
 if __name__ == "__main__":
     num_of_trials = int(input("How many trials do you want to run?\n"))
+
+    # fill up deck
+    for i in range(52):
+        DECK.append(i + 1)
 
     card_simulator(num_of_trials)
